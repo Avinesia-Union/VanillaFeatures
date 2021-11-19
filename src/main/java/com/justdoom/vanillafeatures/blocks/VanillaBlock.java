d9b3ec53f615c7f80b8738bb4cbda8f19bec91c3
Powerbute <64857329+paisendevmc@users.noreply.github.com>
2021-11-19 16:05:31 +0300

    Update VanillaBlock.java

--- src/main/java/com/justdoom/vanillafeatures/blocks/VanillaBlock.java
+++ src/main/java/com/justdoom/vanillafeatures/blocks/VanillaBlock.java
@@ -50,12 +50,6 @@ public abstract class VanillaBlock extends CustomBlock {
         return baseBlock;
     }
 
-     * Create data for this block
-     *
-     * @param blockPosition
-     * @param data
-     * @return
-     *
     @Override
     public Data createData(Instance instance, BlockPosition blockPosition, Data data) {
         return data;
@@ -76,8 +70,6 @@ public abstract class VanillaBlock extends CustomBlock {
 
     }
 
-     * Interact with this block, depending on properties
-     *
     @Override
     public boolean onInteract(Player player, Player.Hand hand, BlockPosition blockPosition, Data data) {
         return false;
@@ -100,18 +92,7 @@ public abstract class VanillaBlock extends CustomBlock {
     public short getVisualBlockForPlacement(Player player, Player.Hand hand, BlockPosition blockPosition) {
         return getBaseBlockId();
     }
-     * Loads the BlockEntity information from the given NBT, during world loading from the Anvil format.
-     * Should be stored in the Data object returned by this function.
-     * It is allowed (and encouraged) to modify 'originalData' and returning it.
-     * <p>
-     * Your method {@link #createData(Instance, BlockPosition, Data)} should return a non-null data object if you want to use this method easily
-     *
-     * @param nbt          the nbt data to read from
-     * @param instance     instance in which the tile entity is being loaded
-     * @param position     position at which this block is. DON'T CACHE IT
-     * @param originalData data present at the current position
-     * @return a Data object with the loaded information. Can be originalData, a new object, or even null if you don't use the TE info
-     *
+
     public Data readBlockEntity(NBTCompound nbt, Instance instance, BlockPosition position, Data originalData) {
         return originalData;
     }
