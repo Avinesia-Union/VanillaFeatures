db4409b83b193bf59c46eadedd08eb70804e657f
Powerbute <64857329+paisendevmc@users.noreply.github.com>
2021-11-19 16:05:00 +0300

    Update BlockStates.java

--- src/main/java/com/justdoom/vanillafeatures/blocks/BlockStates.java
+++ src/main/java/com/justdoom/vanillafeatures/blocks/BlockStates.java
@@ -12,10 +12,6 @@ import java.util.stream.Collectors;
 public class BlockStates {
 
     private final List<BlockState> states;
-
-     * Map that stores a comma-separated list of properties, sorted by alphabetical order as key,
-     * and the corresponding block state as value
-     *
     private final Map<String, BlockState> nameLookup = new HashMap<>();
     private final Short2ObjectOpenHashMap<BlockState> idLookup = new Short2ObjectOpenHashMap<>();
     private final BlockPropertyList properties;
@@ -25,8 +21,6 @@ public class BlockStates {
         this.states = new LinkedList<>();
     }
 
-     * Adds a new blockstate to the known ones
-     *
     void add(BlockState blockState) {
         states.add(blockState);
         String lookupKey = properties.computeSortedList().stream()
@@ -35,12 +29,6 @@ public class BlockStates {
         nameLookup.put(lookupKey, blockState);
         idLookup.put(blockState.getBlockId(), blockState);
     }
-
-     * Gets a BlockState based on the given properties
-     * the value 'value'
-     * @param properties
-     * @return
-     *
     public BlockState getState(Map<String, String> properties) {
         String lookupKey = properties.entrySet().stream()
                 .sorted(Comparator.comparing(Map.Entry::getKey))
@@ -48,22 +36,10 @@ public class BlockStates {
                 .collect(Collectors.joining(","));
         return nameLookup.get(lookupKey);
     }
-
-     * Gets a BlockState based on its protocol id. Return {@link #getDefault()} if none found
-     * @param id
-     * @return
-     *
     public BlockState fromStateID(short id) {
         return idLookup.getOrDefault(id, getDefault());
     }
 
-     * Gets a BlockState based on the given properties, with the property corresponding to 'key' being changed to have
-     * the value 'value'
-     * @param properties
-     * @param key
-     * @param value
-     * @return
-     *
     public BlockState getStateWithChange(Map<String, String> properties, String key, String value) {
         String lookupKey = properties.entrySet().stream()
                 .sorted(Comparator.comparing(Map.Entry::getKey))
@@ -82,12 +58,6 @@ public class BlockStates {
         return states.get(0);
     }
 
-     * Returns the corresponding BlockState at the given position.
-     * Can return null if it does not correspond to any known state.
-     * @param instance
-     * @param blockPosition
-     * @return
-     *
     public BlockState getFromInstance(Instance instance, BlockPosition blockPosition) {
         short id = instance.getBlockStateId(blockPosition);
         BlockAlternative alternative = Block.fromStateId(id).getAlternative(id);
